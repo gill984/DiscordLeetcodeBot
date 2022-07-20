f38361da1f08db8d29e93cbd623313d809a22823
Michael Gill <mjgill984@gmail.com>
2022-07-20 19:09:25 -0400

    Added main test to create title

--- create_title.py
+++ create_title.py
@@ -12,5 +12,5 @@ def TEST_dashed_to_title():
     input = "hi-ok-no-what"
     print(dashed_to_title(input))
 
-# TEST_dashed_to_title()
-# print (url_to_dashed_title('https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/'))
+if __name__ == '__main__':
+    print (url_to_dashed_title('https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/'))
