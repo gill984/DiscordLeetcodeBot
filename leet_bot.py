00fa219fa48d16e5db49d2f61439c01a9aa4d36e
Michael Gill <mjgill984@gmail.com>
2022-07-20 19:44:29 -0400

    Added failure response

--- leet_bot.py
+++ leet_bot.py
@@ -7,17 +7,16 @@ bot = discord.Bot()
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(guild_ids=['997873023589818569'])
-async def hello(ctx):
-    await ctx.respond("Hello!")
-
-@bot.slash_command(guild_ids=['997873023589818569'])
+@bot.slash_command(guild_ids=['215246893758808064'])
 async def leet(ctx):
-    channel = bot.get_channel(997873023589818572)
+    channel = bot.get_channel(877262060625489931)
     problem = scrape_leetcode.get_latest_problem_title()
-    thread = await channel.create_thread(name=problem[1], message=None, auto_archive_duration=60, type=discord.ChannelType.public_thread, reason=None)
-    await thread.send(problem[0])
-    await ctx.respond("Created Thread!")
+    if (problem is None):
+        ctx.respond("Couldn't create thread, sorry!")
+    else:
+        thread = await channel.create_thread(name=problem[1], message=None, auto_archive_duration=60, type=discord.ChannelType.public_thread, reason=None)
+        await thread.send(problem[0])
+        await ctx.respond("Created Thread!")
 
 token_file = open("pw.txt", "r")
 token = token_file.read()
