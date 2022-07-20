49a7da3cc3b20289c54828eacaf87e74a527b6f1
Michael Gill <mjgill984@gmail.com>
2022-07-20 19:20:48 -0400

    Added putting the URL in the thread

--- leet_bot.py
+++ leet_bot.py
@@ -15,8 +15,9 @@ async def hello(ctx):
 async def leet(ctx):
     channel = bot.get_channel(997873023589818572)
     problem = scrape_leetcode.get_latest_problem_title()
-
-    await channel.create_thread(name=problem[1], message=None, auto_archive_duration=60, type=discord.ChannelType.public_thread, reason=None)
+    thread = await channel.create_thread(name=problem[1], message=None, auto_archive_duration=60, type=discord.ChannelType.public_thread, reason=None)
+    await thread.send(problem[0])
+    await ctx.respond("Created Thread!")
 
 token_file = open("pw.txt", "r")
 token = token_file.read()
