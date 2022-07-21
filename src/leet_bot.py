6d4f7d93e8650e72edfd2fcdb848238e69d714c3
Michael Gill <mjgill984@gmail.com>
2022-07-20 21:35:47 -0400

    prevented creating duplicate thread

--- src/leet_bot.py
+++ src/leet_bot.py
@@ -7,16 +7,24 @@ bot = discord.Bot()
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(guild_ids=['215246893758808064'])
+@bot.slash_command(guild_ids=['215246893758808064', '997873023589818569'])
 async def leet(ctx):
-    channel = bot.get_channel(877262060625489931)
+    channel = ctx.channel
+    threads = channel.threads
+
     problem = scrape_leetcode.get_latest_problem_title()
     if (problem is None):
-        ctx.respond("Couldn't create thread, sorry!")
-    else:
-        thread = await channel.create_thread(name=problem[1], message=None, auto_archive_duration=60, type=discord.ChannelType.public_thread, reason=None)
-        await thread.send(problem[0])
-        await ctx.respond("Created Thread!")
+        await ctx.respond("Couldn't create thread, sorry!")
+        return
+    
+    for thread in threads:
+        if thread.name == problem[1]:
+            await ctx.respond("A thread for this problem was already created here: " + thread.jump_url)
+            return
+    
+    new_thread = await channel.create_thread(name=problem[1], message=None, auto_archive_duration=60, type=discord.ChannelType.public_thread, reason=None)
+    await new_thread.send(problem[0])
+    await ctx.respond("Created Thread!")
 
 token_file = open("pw.txt", "r")
 token = token_file.read()
